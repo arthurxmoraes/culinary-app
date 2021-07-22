b2b568279a88a93ff214d4dee585d712128d34e7
Arthur Moraes <ivson.ma@gmail.com>
2021-07-22 12:43:38 -0400

    Created Ingredient model and displayed ingredients list, created shopping list action buttons and modified shopping list edit template

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model'
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
@@ -7,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShoppingListComponent implements OnInit {
 
+  ingredients: Ingredient[] = [
+    new Ingredient('Potato', 5),
+    new Ingredient('Carrot', 2)
+  ];
   constructor() { }
 
   ngOnInit(): void {
